e3c796a24a1680e4b3a8a4420829c7f7d50d34eb
Mohamed Ahmed <108230814+Mohamed101Ahmed@users.noreply.github.com>
2023-07-23 05:16:36 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,78 @@
-provider "aws" {
-  region = "us-west-2"  # Replace with your desired AWS region
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
 }
 
-resource "aws_instance" "web_app_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with the appropriate AMI ID for your region
-  instance_type = "t2.micro"
-  tags = {
-    Name = "my-app-instance"
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "my-resource-group"
+  location = "East US"  # Change this to your desired Azure region
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-virtual-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "my-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "my-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "my-ip-config"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
-# Other infrastructure resources, load balancer, security groups, etc. can be added here.
+# Virtual Machine
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "my-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_DS2_v2"
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "my-vm"
+    admin_username = "azureuser"
+    admin_password = "Password123!"  # Replace with your desired admin password
+  }
+
+  os_profile_windows_config {
+    provision_vm_agent = true
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
